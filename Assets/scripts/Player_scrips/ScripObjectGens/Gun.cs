721317be43d5a0be6964395b269ec8a8ecec77c9
Mehran Ehteshami <c.kiavash.e.s.w@gmail.com>
2019-11-12 19:44:05 +0330

    bullet hole add

---
+++ Assets/scripts/Player_scrips/ScripObjectGens/Gun.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+[CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
+public class Gun : ScriptableObject {
+    public string name;
+    public float firerate;
+    public GameObject prefab;
+	
+}
