23cea140819769c88a1413fb13c1a8828732f9f3
Radman84 <radmanyaghoobi@gmail.com>
2020-02-22 17:35:03 +0330

    Restart

--- Assets/scripts/Player_scrips/ScripObjectGens/Gun.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-[CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
-public class Gun : ScriptableObject {
-    public string name;
-    public float firerate;
-    public GameObject prefab;
-	
-}
